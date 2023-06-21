f428b17dcfb00b382b100ca352d21db3b0209c88
DEV-NKP <niloykanti.paul2017@gmail.com>
2023-06-21 20:56:46 +0600

    .env works done

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ShowsService } from './services/shows.service';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
 import { AdminGuard } from './middlewares/session.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://sayburgh:1234@shows.uwmtkpx.mongodb.net/nodejs-demo?retryWrites=true&w=majority'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     MongooseModule.forFeature([{name:"Shows", schema:ShowsSchema}]),
     MongooseModule.forFeature([{name:"User", schema:UserSchema}]),
   ],
